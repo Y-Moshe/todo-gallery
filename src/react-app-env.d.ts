c6a0b8909d7b99a5dfa3eafa9d6377107e6c7e40
y-moshe <moshe.nehemiah@gmail.com>
2021-08-10 17:52:54 +0300

    add REACT_APP_UNSPLASH_ACCESS_KEY

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    REACT_APP_UNSPLASH_ACCESS_KEY: string;
+  }
+}
+
+interface Window {
+  Stripe: any
+}
