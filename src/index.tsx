b9b7bbcce7c088c3861a7274aefef2fdacae4deb
y-moshe <moshe.nehemiah@gmail.com>
2021-08-19 20:36:12 +0300

    m-ui font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '@fontsource/roboto';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
